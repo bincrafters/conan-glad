5a8915ea34ca511e7f723a5d40834e4f8438d7f6
Philippe Groarke <philippe.groarke@gmail.com>
2018-02-18 01:49:07 -0500

    Working build.

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,10 @@ import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
+    name = "glad"
+    version = "0.1.16a0"
+    url = "https://github.com/bincrafters/conan-glad"
+    description = "Multi-Language GL/GLES/EGL/GLX/WGL Loader-Generator based on the official specs."
 
     # Indicates License type of the packaged library
     license = "MIT"
@@ -24,33 +24,50 @@ class LibnameConan(ConanFile):
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+
+    options = {
+        "profile": ["compatibility", "core"], # OpenGL profile.
+        "api_type": "ANY", # API type like "gl,gles"
+        "api_version": "ANY", # API versionlike "3.2,4.1", no version means latest
+        "extensions": "ANY", # Path to extensions file or comma separated list of extensions, if missing all extensions are included
+        "spec": ["gl","egl","glx","wgl"], # Name of the spec
+        "no_loader": [True, False] # No loader
+    }
+
+    default_options = (
+        "profile=compatibility",
+        "api_type=gl",
+        "api_version=3.2",
+        "extensions=",
+        "spec=gl",
+        "no_loader=False"
+    )
 
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
+    # requires = ()
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/Dav1dde/glad"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
         #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
+
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.definitions["GLAD_PROFILE"] = self.options.profile
+        cmake.definitions["GLAD_API"] = "%s=%s" % (self.options.api_type, self.options.api_version)
+        cmake.definitions["GLAD_EXTENSIONS"] = self.options.extensions
+        cmake.definitions["GLAD_SPEC"] = self.options.spec
+        cmake.definitions["GLAD_NO_LOADER"] = self.options.no_loader
+        cmake.definitions["GLAD_GENERATOR"] = "c"
+        cmake.definitions["GLAD_EXPORT"] = True
+        cmake.definitions["GLAD_INSTALL"] = True
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
@@ -58,14 +75,15 @@ class LibnameConan(ConanFile):
     def package(self):
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        # include_folder = os.path.join(self.source_subfolder, "include")
+        # self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        # self.copy(pattern="*", dst="include", src=include_folder)
+        # self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        # self.copy(pattern="*.lib", dst="lib", keep_path=False)
+        # self.copy(pattern="*.a", dst="lib", keep_path=False)
+        # self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        # self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        pass
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
