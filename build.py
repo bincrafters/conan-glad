d943500f7089a0a775b40a39fecfa500edf5ef06
Philippe Groarke <philippe.groarke@gmail.com>
2018-02-17 22:59:30 -0500

    Add conan template.

---
+++ build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+
+from bincrafters import build_template_default
+
+if __name__ == "__main__":
+
+    builder = build_template_default.get_builder()
+
+    builder.run()
